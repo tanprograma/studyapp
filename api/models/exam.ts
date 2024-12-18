01d5bc32678c8b8f46fdfa15e78a9d5af56d11c1
tanprograma <tanprograma@gmail.com>
2024-12-18 19:04:13 +0300

    fixed data in database

--- api/models/exam.ts
+++ api/models/exam.ts
@@ -1,13 +1,11 @@
 import { Schema, model, Types } from 'mongoose';
 import { Exam } from '../../src/app/interfaces/exam';
 
-const schema = new Schema<Exam>(
-  {
-    book: String,
-    topic: String,
-    questions: [{ sn: Number, option: String, selected: String }],
-  },
-  { timestamps: true }
-);
+const schema = new Schema<Exam>({
+  book: String,
+  topic: String,
+  subject: String,
+  questions: [{ sn: Number, option: String, selected: String }],
+});
 const mymodel = model<Exam>('Exam', schema);
 export default mymodel;
